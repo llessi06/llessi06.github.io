ffecda0b391ab951e541368a383df1164b1cd712
Lukas Leßlhumer <lukaslesslhumer6@gmail.com>
2024-07-14 20:37:45 +0200

    Revert "Hash Router"
    This reverts commit 3016c1036fe1bfe155ab27a8c2b4580ebb0b3519.

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import IndexPage from "./pages/index.jsx";
+import {Router} from "@reach/router";
 import NotFoundPage from "./pages/404.jsx";
 import PrivacyPolicy from "./pages/privacy-policy.jsx";
 import Imprint from "./pages/imprint.jsx";
-import {HashRouter, Route, Routes} from "react-router-dom";
-import IndexPage from "./pages/index.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>
-    <HashRouter basename={"/"}>
-        <Routes>
-            <Route exact path="/" element={<IndexPage/>}/>
-            <Route exact path="/privacy-policy" element={<PrivacyPolicy/>}/>
-            <Route exact path="/imprint" element={<Imprint/>}/>
-            <Route element={<NotFoundPage/>}/>
-        </Routes>
-    </HashRouter>
-</React.StrictMode>)
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Router>
+            <IndexPage path="/"/>
+            <PrivacyPolicy path="/privacy-policy"/>
+            <Imprint path="/imprint"/>
+            <NotFoundPage default/>
+        </Router>
+    </React.StrictMode>
+)
