b061b70dd5292b8615f329ef2eef91e5728c5e75
Lukas Leßlhumer <lukaslesslhumer6@gmail.com>
2024-07-14 18:33:32 +0200

    [TEST] Using static html links

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -100,10 +100,10 @@ const Footer = () => {
 
             <StyledPrivacyImprintWrapper>
                 <StyledPrivacyPolicy>
-                    <a href="/privacy-policy">Privacy Policy</a>
+                    <a href="/privacy-policy.html">Privacy Policy</a>
                 </StyledPrivacyPolicy>
                 <StyledImprint>
-                    <a href="/imprint">Imprint</a>
+                    <a href="/imprint.html">Imprint</a>
                 </StyledImprint>
             </StyledPrivacyImprintWrapper>
 
